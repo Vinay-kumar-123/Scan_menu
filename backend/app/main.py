8c1284db2e7e3e834695921a2d15d5eff31adfd2
vinay-kumar-123 <kingboy92785@gmail.com>
2026-03-19 15:21:36 +0530

    cors update

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,14 +3,16 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routes import auth,store, product,order,subscription,webhook
 app = FastAPI()
 
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://scanbite-five.vercel.app",
     ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
