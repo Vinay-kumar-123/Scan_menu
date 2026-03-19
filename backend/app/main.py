deef6d66b0aec7e6bd44799b72a5c47f6056e98f
vinay-kumar-123 <kingboy92785@gmail.com>
2026-03-19 11:05:27 +0530

    cors update

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,10 +7,13 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # 🔥 TEMP FULL OPEN
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
     allow_credentials=True,
-    allow_methods=["*"],   # 🔥 VERY IMPORTANT
-    allow_headers=["*"],   # 🔥 VERY IMPORTANT
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 app.include_router(auth.router)
 app.include_router(store.router)
